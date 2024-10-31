9808a94ecbcd0ec1c1e09e8e33d3b026ce605f4f
Aryan Raj <aryaraj132@gmail.com>
2024-11-01 02:46:33 +0530

    update dotfile setups

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+rm -rf ~/.zshrc
+rm -rf ~/.gitconfig
+rm -rf ~/.config/nvim
+rm -rf ~/.config/alacritty
+
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 mkdir ~/.config/nvim/
