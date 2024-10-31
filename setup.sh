0fed11d6fd19c39f4a79d71c7fe6174262272600
Aryan Raj <aryaraj132@gmail.com>
2024-10-31 21:24:07 +0000

    don't create folders when creating links

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ rm -rf ~/.config/alacritty
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-mkdir ~/.config/nvim/
 ln -s ~/.dotfiles/.config/nvim ~/.config
-mkdir ~/.config/alacritty/
 ln -s ~/.dotfiles/.config/alacritty/ ~/.config
